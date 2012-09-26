2feec557271bfcc267ca64be967f0b430ec7ee17
Andrea Jenkins <andrea.r.jenkins@gmail.com>
2012-09-26 10:21:11 -0400

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniLms::Application.config.secret_token = '6dc0c235d4d596ccad07401557c7d56b838bc917771714023da0ffa979c06d66f3d389167ee4087c4a0b5e12818ec9fc973fc63acef3a170c5ec6135ed359ead'
