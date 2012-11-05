29a457e0b8621f646e8feb49ab173fedf4740045
Andrea Jenkins <andrea.r.jenkins@gmail.com>
2012-11-05 11:48:46 -0500

    tweaked instructor order

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -5,7 +5,7 @@ class InstructorsController < ApplicationController
   skip_before_filter :check_access, :except => :create_photo
   
   def index
-    @instructors = Instructor.all
+    @instructors = Instructor.order("firstname DESC")
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @instructors }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   
   
   def index
-    @users = User.all
+    @users = User.order("firstname DESC")
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
